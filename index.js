4368341c8f636478c75d6ad8883256a4629e356e
Julian Bull <julianbullmagic@gmail.com>
2021-12-10 16:57:00 +1100

    ffff

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const http = require('http');
 const express = require('express');
-const socketio = require('socket.io');
 const cors = require('cors');
 
 const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
@@ -9,62 +8,95 @@ const router = require('./router');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server);
+
+const io = require("socket.io")(server, {
+	cors: {
+		origin: "http://localhost:3000"||"http://robot-socket-backend.herokuapp.com",
+		methods: [ "GET", "POST" ]
+	}
+})
 
 app.use(cors());
 app.use(router);
 
 
 io.on('connect', (socket) => {
-  socket.on('join', ({ name, room }, callback) => {
-    const { error, user } = addUser({ id: socket.id, name, room });
+  socket.emit("me", socket.id)
 
-    if (error) return callback(error);
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("callEnded")
+  })
 
-    socket.join(user.room);
+  socket.on("callUser", (data) => {
+    io.to(data.userToCall).emit("callUser", { signal: data.signalData, from: data.from, name: data.name })
+  })
 
-    socket.emit('message', {
-      user: 'admin',
-      text: `${user.name}, welcome to room ${user.room}.`
-    });
-    socket.broadcast
-      .to(user.room)
-      .emit('message', { user: 'admin', text: `${user.name} has joined!` });
-
-    io.to(user.room).emit('roomData', {
-      room: user.room,
-      users: getUsersInRoom(user.room)
-    });
-
-    callback();
-  });
-
-  socket.on('sendMessage', (message, callback) => {
-    const user = getUser(socket.id);
-
-    io.to(user.room).emit('message', { user: user.name, text: message });
-    io.to(user.room).emit('roomData', {
-      room: user.room,
-      users: getUsersInRoom(user.room)
-    });
-
-    callback();
-  });
-
-  socket.on('disconnect', () => {
-    const user = removeUser(socket.id);
-
-    if (user) {
-      io.to(user.room).emit('message', {
-        user: 'Admin',
-        text: `${user.name} has left.`
-      });
-      io.to(user.room).emit('roomData', {
-        room: user.room,
-        users: getUsersInRoom(user.room)
-      });
-    }
-  });
+  socket.on("answerCall", (data) => {
+    io.to(data.to).emit("callAccepted", data.signal)
+  })
+  // socket.emit("me", socket.id)
+  //
+  //
+  // socket.on("callUser", (data) => {
+  //   io.to(data.userToCall).emit("callUser", { signal: data.signalData, from: data.from, name: data.name })
+  // })
+  //
+  // socket.on("answerCall", (data) => {
+  //   io.to(data.to).emit("callAccepted", data.signal)
+  // })
+  //
+  //
+  //
+  // socket.on('join', ({ name, room }, callback) => {
+  //   const { error, user } = addUser({ id: socket.id, name, room });
+  //
+  //   if (error) return callback(error);
+  //
+  //   socket.join(user.room);
+  //
+  //   socket.emit('message', {
+  //     user: 'admin',
+  //     text: `${user.name}, welcome to room ${user.room}.`
+  //   });
+  //   socket.broadcast
+  //     .to(user.room)
+  //     .emit('message', { user: 'admin', text: `${user.name} has joined!` });
+  //
+  //   io.to(user.room).emit('roomData', {
+  //     room: user.room,
+  //     users: getUsersInRoom(user.room)
+  //   });
+  //
+  //   callback();
+  // });
+  //
+  // socket.on('sendMessage', (message, callback) => {
+  //   const user = getUser(socket.id);
+  //
+  //   io.to(user.room).emit('message', { user: user.name, text: message });
+  //   io.to(user.room).emit('roomData', {
+  //     room: user.room,
+  //     users: getUsersInRoom(user.room)
+  //   });
+  //
+  //   callback();
+  // });
+  //
+  // socket.on('disconnect', () => {
+  //   const user = removeUser(socket.id);
+  //   socket.broadcast.emit("callEnded")
+  //
+  //   if (user) {
+  //     io.to(user.room).emit('message', {
+  //       user: 'Admin',
+  //       text: `${user.name} has left.`
+  //     });
+  //     io.to(user.room).emit('roomData', {
+  //       room: user.room,
+  //       users: getUsersInRoom(user.room)
+  //     });
+  //   }
+  // });
 });
 
 server.listen(process.env.PORT || 5000, () =>
