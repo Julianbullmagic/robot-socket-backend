6f6bf7649b2ced33d60cf5de612408c34a39fd8d
Julian Bull <julianbullmagic@gmail.com>
2021-12-11 13:34:59 +1100

    fffff

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 
 const io = require("socket.io")(server, {
 	cors: {
-		origin: "https://robot-socket.herokuapp.com"||"http://localhost:3000",
+		origin: "https://robot-socket.herokuapp.com"||"https://localhost:3000"||"https://robot-socket-server.herokuapp.com",
 		methods: [ "GET", "POST" ]
 	}
 })
@@ -21,82 +21,58 @@ app.use(router);
 
 
 io.on('connect', (socket) => {
-  socket.emit("me", socket.id)
 
-  socket.on("disconnect", () => {
+
+  socket.on('join', ({ name, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, name, room });
+
+    if (error) return callback(error);
+
+    socket.join(user.room);
+
+    socket.emit('message', {
+      user: 'admin',
+      text: `${user.name}, welcome to room ${user.room}.`
+    });
+    socket.broadcast
+      .to(user.room)
+      .emit('message', { user: 'admin', text: `${user.name} has joined!` });
+
+    io.to(user.room).emit('roomData', {
+      room: user.room,
+      users: getUsersInRoom(user.room)
+    });
+
+    callback();
+  });
+
+  socket.on('sendMessage', (message, callback) => {
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit('message', { user: user.name, text: message });
+    io.to(user.room).emit('roomData', {
+      room: user.room,
+      users: getUsersInRoom(user.room)
+    });
+
+    callback();
+  });
+
+  socket.on('disconnect', () => {
+    const user = removeUser(socket.id);
     socket.broadcast.emit("callEnded")
-  })
 
-  socket.on("callUser", (data) => {
-    io.to(data.userToCall).emit("callUser", { signal: data.signalData, from: data.from, name: data.name })
-  })
-
-  socket.on("answerCall", (data) => {
-    io.to(data.to).emit("callAccepted", data.signal)
-  })
-  // socket.emit("me", socket.id)
-  //
-  //
-  // socket.on("callUser", (data) => {
-  //   io.to(data.userToCall).emit("callUser", { signal: data.signalData, from: data.from, name: data.name })
-  // })
-  //
-  // socket.on("answerCall", (data) => {
-  //   io.to(data.to).emit("callAccepted", data.signal)
-  // })
-  //
-  //
-  //
-  // socket.on('join', ({ name, room }, callback) => {
-  //   const { error, user } = addUser({ id: socket.id, name, room });
-  //
-  //   if (error) return callback(error);
-  //
-  //   socket.join(user.room);
-  //
-  //   socket.emit('message', {
-  //     user: 'admin',
-  //     text: `${user.name}, welcome to room ${user.room}.`
-  //   });
-  //   socket.broadcast
-  //     .to(user.room)
-  //     .emit('message', { user: 'admin', text: `${user.name} has joined!` });
-  //
-  //   io.to(user.room).emit('roomData', {
-  //     room: user.room,
-  //     users: getUsersInRoom(user.room)
-  //   });
-  //
-  //   callback();
-  // });
-  //
-  // socket.on('sendMessage', (message, callback) => {
-  //   const user = getUser(socket.id);
-  //
-  //   io.to(user.room).emit('message', { user: user.name, text: message });
-  //   io.to(user.room).emit('roomData', {
-  //     room: user.room,
-  //     users: getUsersInRoom(user.room)
-  //   });
-  //
-  //   callback();
-  // });
-  //
-  // socket.on('disconnect', () => {
-  //   const user = removeUser(socket.id);
-  //   socket.broadcast.emit("callEnded")
-  //
-  //   if (user) {
-  //     io.to(user.room).emit('message', {
-  //       user: 'Admin',
-  //       text: `${user.name} has left.`
-  //     });
-  //     io.to(user.room).emit('roomData', {
-  //       room: user.room,
-  //       users: getUsersInRoom(user.room)
-  //     });
-  //   }
-  // });
+    if (user) {
+      io.to(user.room).emit('message', {
+        user: 'Admin',
+        text: `${user.name} has left.`
+      });
+      io.to(user.room).emit('roomData', {
+        room: user.room,
+        users: getUsersInRoom(user.room)
+      });
+    }
+  });
 });
 
 server.listen(process.env.PORT || 5000, () =>
