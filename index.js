b60cde313ee550946edcd22ba8da614f072cf6d6
Julian Bull <julianbullmagic@gmail.com>
2021-12-12 20:56:24 +1100

    ffff

--- index.js
+++ index.js
@@ -53,6 +53,14 @@ io.on('connect', (socket) => {
     callback();
   });
 
+
+	socket.on('camera joined', (message, callback) => {
+    const user = getUser(socket.id);
+    io.to(user.room).emit('message', { user: "admin", text: message });
+
+    callback();
+  });
+
   socket.on('sendMessage', (message, callback) => {
     const user = getUser(socket.id);
     io.to(user.room).emit('message', { user: user.name, text: message });
